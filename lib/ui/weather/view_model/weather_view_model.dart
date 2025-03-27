007912d9f06bbc3385cb7b8eabe5be32692de23e
r0227n <developryo@gmail.com>
2025-03-27 19:19:55 +0900

    migrate: notifierでWeatherの状態管理に変更

---
+++ lib/ui/weather/view_model/weather_view_model.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_training/domain/models/weather.dart';
+import 'package:flutter_training/domain/use_case/weather/weather_forecast_use_case.dart';
+import 'package:flutter_training/utils/result.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'weather_view_model.g.dart';
+
+@riverpod
+class WeatherViewModel extends _$WeatherViewModel {
+  @override
+  Weather? build() {
+    return null;
+  }
+
+  final _weatherUseCase = WeatherForecastUseCase();
+
+  void updateWeather({WeatherArea area = WeatherArea.tokyo, DateTime? date}) {
+    final input = InputWeatherForecast(
+      area: area,
+      date: date ?? DateTime.now(),
+    );
+
+    final weatherCondition = _weatherUseCase.getWeather(input);
+
+    switch (weatherCondition) {
+      case Success<Weather, Exception>():
+        state = weatherCondition.success;
+      case Error<Weather, Exception>():
+        throw weatherCondition.error;
+    }
+  }
+}
---
+++ lib/ui/weather/view_model/weather_view_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherViewModelHash() => r'1779206ef9dcf2bb2be5f4938d4930b029f292ec';
+
+/// See also [WeatherViewModel].
+@ProviderFor(WeatherViewModel)
+final weatherViewModelProvider =
+    AutoDisposeNotifierProvider<WeatherViewModel, Weather?>.internal(
+      WeatherViewModel.new,
+      name: r'weatherViewModelProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$weatherViewModelHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$WeatherViewModel = AutoDisposeNotifier<Weather?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
