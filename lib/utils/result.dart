cb9be57039b20a40e10b7f694f6eabbe7d7def04
r0227n <developryo@gmail.com>
2025-03-28 13:15:07 +0900

    chorc: Result EをException拡張しないようにした

--- lib/utils/result.dart
+++ lib/utils/result.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'result.freezed.dart';
 
 @freezed
-sealed class Result<T, E extends Exception> with _$Result<T, E> {
+sealed class Result<T, E> with _$Result<T, E> {
   const factory Result.success(T success) = Success<T, E>;
   const factory Result.error(E error) = Error<T, E>;
 }
